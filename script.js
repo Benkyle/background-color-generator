a7995de3804d4dbb182079542f1ebefc7a8467c4
Benkyle <benedicamous@gmail.com>
2022-08-04 16:06:54 +0100

    updated

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 var css = document.querySelector("h3");
-var color1 = document.querySelector("color1");
-var color2 = document.querySelector("color2");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
 
-console.log(css);
-console.log(color1);
-console.log(color2);
 
-color1.addEventListener("input" , function () {
-    console.log(color1.value);
-})
+//because of dry(do not repeat urself we cant use this although it works)
+// color1.addEventListener("input" , function () {
+// //     body.style.background = 
+// //     "linear-gradient(to right," 
+// //     + color1.value 
+// //     +" , " 
+// //     + color2.value 
+// //     +")";
+// // })
 
-color.addEventListener("input" , function () {
-    console.log(color2.value);
-})
+// // color2.addEventListener("input" , function () {
+// //     body.style.background = 
+// //     "linear-gradient(to right," 
+// //     + color1.value 
+// //     +" , " 
+// //     + color2.value 
+// //     +")";
+// // })
+
+
+function setGradient() {
+    body.style.background = 
+    "linear-gradient(to right," 
+    + color1.value 
+    +" , " 
+    + color2.value 
+    +")";
+}
+color1.addEventListener("input" , setGradient);
+color2.addEventListener("input" , setGradient);
